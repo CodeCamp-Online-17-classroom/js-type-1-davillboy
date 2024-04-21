29d50e88bd0e966ef07f0c8b1b81ce7d0ad5d0a9
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 23:35:19 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+
+        let a = undefined;
+        let b = 2;
+        let c = a++;
+
+        
+        let d = String(a);
+        let e = String(b);
+        let f = String(c);
+
+        console.log(d); 
+        console.log(e); 
+        console.log(f); 
